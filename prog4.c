9b4d71798c5919f470f16cfdfdd61a3e0b2b83c2
Nalapriya <52316295+Nalapriya@users.noreply.github.com>
2019-08-13 19:06:39 +0530

    Create prog4.c

---
+++ prog4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+    {
+        printf("Alphabet");
+    }
+    else
+    {
+        printf("Not an Alphabet");
+
+    }
+    return 0;
+}
