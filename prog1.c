91accbf33595009958a784f0ac947403aed74e30
Nalapriya <52316295+Nalapriya@users.noreply.github.com>
2019-08-13 18:30:04 +0530

    Create prog1.c

---
+++ prog1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+    else if(n<0)
+    {
+
+    printf("negative");
+    }
+    else
+    {
+        printf("Zero");
+    }
+    return 0;
+}
