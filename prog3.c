11b722903abadff6587a10a1165c00ad9f79c0b5
Nalapriya <52316295+Nalapriya@users.noreply.github.com>
2019-08-13 18:59:24 +0530

    Update prog3.c

--- prog3.c
+++ prog3.c
@@ -2,9 +2,9 @@
 #include<conio.h>
 int main()
 {
-    char ch;
-    scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    char char;
+    scanf("%c",&char);
+    if(char=='a'||char=='e'||char=='i'||char=='o'||char=='u'||char=='A'||char=='E'||char=='I'||char=='O'||char=='U')
     {
         printf("Vowel");
     }
